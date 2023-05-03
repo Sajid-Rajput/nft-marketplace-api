48f4a96e6e76ff95ffc5d3cb38f93497e99d996b
Muhammad Sajid Ali Khan <SajidRajput01.and@gmail.com>
2023-05-04 00:54:16 +0500

    encrypt user password using bcrypt

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 import validator from "../Utils/validator.js";
 
@@ -38,6 +39,30 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre(
+  "save",
+  async function (
+    this: mongoose.Document & {
+      password: string;
+      passwordConfirm: string | undefined;
+    },
+    next
+  ) {
+    // THE GIVEN IF STATEMENT IS RUNNING ONLY AT THAT TIME WHEN ACCOUNT IS UPDATING TO PASS THE PASSWORD ENCRYPTION PART
+    if (!this.isModified("password")) {
+      return next();
+    }
+    // IF THE ABOVE IF STATEMENT IS FALSE THEN THE GIVEN CODE IS RUNNING
+    this.password = await bcrypt.hash(this.password, 12);
+    // DELETE THE CONFIRM PASSWORD FROM THE DATABASE
+    this.passwordConfirm = undefined;
+  }
+);
+
 const USER = mongoose.model("User", userSchema);
+
+//=========================================================================================
+// <- EXPORTS ->
+//=========================================================================================
 
 export default USER;
